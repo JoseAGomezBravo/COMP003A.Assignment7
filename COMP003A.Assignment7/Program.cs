8858b0b85e10000361d2456939d1fa03f491e8fb
Jose Angel Gomez Bravo <joseangelgomezbravo@my.whccd.edu>
2023-03-01 20:40:26 -0800

    Add project files.

---
+++ COMP003A.Assignment7/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Author: Jose Angel Gomez Bravo
+ * Course: COMP-003
+ * Purpose: Replicate the output for the assignment 
+ */
+
+using System.Diagnostics.Metrics;
+
+namespace COMP003A.Assignment7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSeparator("Array - CharacterCounter");
+        }
+
+        /// <summary>
+        /// Section Separator method
+        /// </summary>
+        /// <param name="title">String input for title name</param>
+        static void PrintSeparator(string title)
+        {
+            Console.WriteLine("".PadRight(50, '*'));
+            Console.WriteLine($"\t{title} Section");
+            Console.WriteLine("".PadRight(50, '*'));
+        }
+
+
+        static int CharacterCounter(char characterInput, string word)
+        {
+            int count = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                characterInput = char.ToLower(characterInput);
+                word = word.ToLower();
+
+                if (word[i] == characterInput)
+                    count ++;
+            }
+            return count;
+        }
+
+
+        static bool IsPalindrome(string word) 
+        {
+            
+        }
+    }
+}
